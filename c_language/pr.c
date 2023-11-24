d84ea802217c35c5b02b728cdd05a496349b2dc8
Mourya <styooodnt@gmail.com>
2023-11-24 14:50:43 +0530

    Update pr.c

--- c_language/pr.c
+++ c_language/pr.c
@@ -1,94 +1,95 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-
-typedef struct {
-    float at;
-    float wt;
-    float bt;
-    int id;
-    float pt;
-    
-}P;
-
-void main(){
+void main() {
     int n;
-    printf("Enter number of process:-  ");
-    scanf("%d",&n);
 
-    P p[n];
+    printf("\nEnter the number of processes: ");
+    scanf("%d", &n);
 
-    for(int k=0;k<n;k++){
-        printf("Enter priority  of  process %d :-  ",k+1);
-        scanf("%f",&p[k].at);
-        printf("Enter burst time  of  process %d :-  ",k+1);
-        scanf("%f",&p[k].bt);
-        p[k].id=k+1;
+    int bt[n];
+    int priority[n];
+    int wt[n];
+    int tat[n];
+    float awt = 0;
+    float atat = 0;
 
+    printf("\nEnter the burst time: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &bt[i]);
     }
-    P temp;
-    for(int t=0;t<n-1;t++){
-        for(int o=0;o<n-t-1;o++){
-            if(p[o].at >p[o+1].at){
-                temp= p[o];
-                p[o]=p[o+1];
-                p[o+1]=temp;
 
+    printf("\nEnter the priority: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &priority[i]);
+    }
+
+    // Structure to represent a process with attributes
+    struct Process {
+        int pid; // Process ID
+        int burstTime; // BurstTime
+        int priority; // Priority
+    };
+
+    // Create an array of processes
+    struct Process p[n];
+
+    for (int i = 0; i < n; i++) {
+        p[i].pid = i;
+        p[i].burstTime = bt[i];
+        p[i].priority = priority[i];
+    }
+
+    // Sort processes based on priority (non-preemptive)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (p[j].priority > p[j + 1].priority) {
+                // Swap the processes
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
         }
     }
 
-  for(int i=0;i<n;i++){
-p[i].pt=p[i].at;
-p[i].at=0;
-
-}
-
-  for(int i=0;i<n;i++){
-    if(i==0){
-    p[i].wt=0;
-    }
-    else{
-        if(p[i].at>p[i-1].wt+p[i-1].bt){
-            p[i].wt=0;
-        }
-        else{
-            p[i].wt=p[i-1].wt+p[i-1].at +p[i-1].bt-p[i].at;
-        }
-    }
-    
-  }
-    float wt=0;
-    float bt=0;
-    for(int i=0;i<n;i++){
-    wt= wt+p[i].wt;
-    bt=bt+p[i].wt+p[i].bt;
+    // Calculate waiting time and turnaround time
+    wt[0] = 0;
+    tat[0] = p[0].burstTime;
+    int comTime = p[0].burstTime; // Completion Time
+    for (int i = 1; i < n; i++) {
+        wt[i] = comTime;
+        comTime += p[i].burstTime;
+        tat[i] = wt[i] + p[i].burstTime;
     }
 
-        for(int t=0;t<n-1;t++){
-        for(int o=0;o<n-t-1;o++){
-            if(p[o].id >p[o+1].id){
-                temp= p[o];
-                p[o]=p[o+1];
-                p[o+1]=temp;
+    // Print the sorted table
+    printf("Sorted Process Table\n");
+    printf("Process\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\n");
 
-            }
-        }
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\n", p[i].pid, p[i].burstTime,
+               p[i].priority, wt[i], tat[i]);
+        awt += wt[i];
+        atat += tat[i];
     }
 
+    awt /= n;
+    atat /= n;
 
-    printf("\nPID \t PT \t\t BT \t\t WT \t\t TT\n");
-    for(int i=0;i<n;i++){
-    printf("%d \t %f \t %f \t %f \t %f\n",p[i].id,p[i].pt,p[i].bt,p[i].wt,p[i].wt+p[i].bt);
-   
+    printf("\nAverage waiting time = %.2f", awt);
+    printf("\nAverage turnaround time = %.2f\n", atat);
+
+    // Gantt Chart
+    printf("\nGantt Chart:\n");
+    for (int i = 0; i < n; i++) {
+        printf("|  P%d  ", p[i].pid);
     }
+    printf("|\n");
 
-    bt=bt/n;
-    wt=wt/n;
-
-    printf("Average waiting time for process is %f\n",wt);
-    printf("Average Turnaround time for process is %f\n",bt);
-
-
-
+    // Current Time
+    int ct = 0;
+    for (int i = 0; i < n; i++) {
+        printf("%d      ", ct);
+        ct += p[i].burstTime;
+    }
+    printf("%d\n", ct);
 }
